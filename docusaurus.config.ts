48b8689f8b9a26d81d4a1ec55302c8bd03809312
yanadev <1245485816@qq.com>
2024-10-12 01:08:39 +0800

    feat：update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,22 +3,22 @@ import type { Config } from '@docusaurus/types'
 import { themes as prismThemes } from 'prism-react-renderer'
 
 const config: Config = {
-  title: 'Qiehaha',
+  title: 'Qiehh',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://qiehh.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
+  organizationName: 'qiehh', // Usually your GitHub org/user name.
+  projectName: 'qiehh.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -124,7 +124,7 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/qiehh/',
             },
           ],
         },
